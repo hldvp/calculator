c4b66d6adb1d0455cf0a136743f6931ea36f7fbd
Polina Kholodova <hldvp@yandex.ru>
2021-04-15 00:42:43 +0300

    класс определения вида входных данных

---
+++ src/com/company/Chek.java
@@ -0,0 +1,41 @@
+package com.company;
+
+public class Chek {
+    public String str;
+    public String flag;
+    public Rim rim;
+    public Arab arab;
+
+    Chek (String str){
+        this.str = str;
+        if (str.contains(" ")){
+            this.str = str.trim();
+        }
+        this.flag = "Arab";
+        String regex = "\\d+";
+        if (!this.str.matches(regex)) this.flag = "No Arab";
+
+        if (this.flag == "No Arab") {
+            this.flag = "Rim";
+            for (int i=0; i<this.str.length(); i++){
+                try {
+                    RomanNumeral r;
+                    r = RomanNumeral.valueOf(String.valueOf(str.charAt(i)));
+                    break;
+                }
+                catch (Exception e){
+                    this.flag = "No Rim";
+                    System.out.println("The data is incorrect");
+                    System.exit(0);
+                }
+            }
+        }
+        if (this.flag == "Arab"){
+            this.arab = new Arab(this.str);
+        }
+        if (this.flag == "Rim"){
+            this.rim = new Rim(this.str);
+        }
+    }
+}
+
