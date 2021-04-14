47884ed7c50381c280a2a625669f130f51f5c98c
Polina Kholodova <hldvp@yandex.ru>
2021-04-14 19:47:30 +0300

    Абстрактный класс для входных данных

---
+++ src/com/company/Abstract.java
@@ -0,0 +1,19 @@
+package com.company;
+
+
+public abstract class Abstract {
+    public  int value;
+
+    public Abstract(int value){
+        this.value = value;
+    }
+
+    protected Abstract() {
+    }
+
+
+    abstract public String Vyvod();
+
+
+
+}
